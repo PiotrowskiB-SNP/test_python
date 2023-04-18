a035aa757a29cd11234edaab75d02066dcca2902
Bartosz <bartosz.piotrowski@all-for-one.com>
2023-04-18 13:01:56 +0200

    Add test with fixture

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,18 @@
 import pytest
 
 
+@pytest.fixture
+def int_fixture() -> int:
+    return 1
+
+
 def test_always_possitive():
     assert True
 
 
 def test_something_positive():
     assert True
+
+
+def test_use_fixture(int_fixture):
+    assert int_fixture + 15 == 16
